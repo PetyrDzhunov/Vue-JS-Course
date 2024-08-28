1c28e9911a192c89fd188def7420f5f5f80e2d31
petardzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2024-08-28 15:17:29 +0300

    v-html directive & vue methods

--- Basics/app.js
+++ Basics/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'asd',
+      courseGoalA: 'First Course Goal',
+      courseGoalB: '<h2>Second Course Goal</h2>',
       vueLink: 'https://vuejs.org/',
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
   },
 });
 
