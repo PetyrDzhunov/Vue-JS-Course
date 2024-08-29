83760a9e67b4c2e71b365b412b62ca3f13c63134
petardzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2024-08-29 15:46:09 +0300

    Using native event object/ passing arguments to event listeners

--- Basics-Vue-Events/app.js
+++ Basics-Vue-Events/app.js
@@ -2,14 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
   methods: {
-    incrementCounter() {
-      this.counter++;
+    incrementCounter(number) {
+      this.counter += number;
     },
-    decrementCounter() {
-      this.counter--;
+    decrementCounter(number) {
+      this.counter -= number;
+    },
+    setName(e) {
+      this.name = e.target.value;
     },
   },
 });
